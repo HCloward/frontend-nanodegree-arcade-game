0a4b8a9f02ea5a1e2926c908af7bc0bba6fc1c3b
Heather Cloward <heathercloward@gmail.com>
2018-06-27 13:21:47 -0400

    fixed game

--- js/app.js
+++ js/app.js
@@ -6,7 +6,16 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x;
+    this.y;
+    this.speed;
+    this.startPosition();
 };
+Enemy.prototype.startPosition = function() {
+    this.x = -101;
+    this.y = Math.floor(Math.random() * 4) * 85 + 60;
+    this.speed = Math.floor(Math.random() * 50) + 90;
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +23,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x > 504) {
+        this.startPosition();
+    } else {
+        this.x += this.speed * dt;
+    }
+    // Check for player collision
+    if (this.y === player.y) {
+        if (this.x + 81 > player.x && this.x < player.x + 71) player.startPosition();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +42,90 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var Player = function() {
+    this.x;
+    this.y;
+    this.sprite = 'images/char-pink-girl.png';
+    this.startPosition();
+}
+Player.prototype.startPosition = function() {
+    this.x = 202;
+    this.y = 400;
+}
+Player.prototype.update = function(dt) {
+    //
+}
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+Player.prototype.handleInput = function(direction) {
+    switch(direction) {
+        case 'left':
+            if (this.x > 100) this.x -= 101;
+            break;
+        case 'up':
+            if (this.y > 60) this.y -= 85;
+            else {
+                this.y -= 85;
+                winAlert();
+                this.startPosition();
+            }
+            break;
+        case 'right':
+            if (this.x < 404) this.x += 101;
+            break;
+        case 'down':
+            if (this.y < 400) this.y += 85;
+            break;
+        default:
+    }
+}
+/*
+player = {
+    x:0,
+    y:0,
+    sprite: 'images/char-boy.png',
+    startPosition: function() {
+        this.x = 202;
+        this.y = 400;
+    },
+    update: function(dt) {  },
+    render: function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    },
+    handleInput: function(direction) {
+        switch(direction) {
+            case 'left':
+                if (this.x > 100) this.x -= 101;
+                break;
+            case 'up':
+                if (this.y > 60) this.y -= 85;
+                else this.startPosition();
+                break;
+            case 'right':
+                if (this.x < 404) this.x += 101;
+                break;
+            case 'down':
+                if (this.y < 400) this.y += 85;
+                break;
+            default:
+        }
+    }
+}
+*/
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+for (let i=0; i<5; i++) {
+    allEnemies.push(new Enemy());
+}
+var player = new Player();
 
-
+function winAlert() {
+    alert('You win!');
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
