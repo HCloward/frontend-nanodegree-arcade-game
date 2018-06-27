926afe1261301243dc464eafacd0d375e1894d95
Heather Cloward <heathercloward@gmail.com>
2018-06-27 13:39:55 -0400

    added comments to code to explain updates

--- js/app.js
+++ js/app.js
@@ -6,13 +6,18 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    // variables for the x and y position of the enemies
     this.x;
     this.y;
+    // speed is a value you add to the enemies x in order to move them across the screen
     this.speed;
+    // set position of where enemy starts
     this.startPosition();
 };
 Enemy.prototype.startPosition = function() {
+    // the enemies' x starting position is negative bc they start off screen
     this.x = -101;
+    // the random function randomly decides which row the enemy will start in (0-3)
     this.y = Math.floor(Math.random() * 4) * 85 + 60;
     this.speed = Math.floor(Math.random() * 50) + 90;
 }
@@ -23,12 +28,16 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // checks if ememy is off screen to move it to the start position
+    // if enemy is not off screen add speed to the x position
     if (this.x > 504) {
         this.startPosition();
     } else {
         this.x += this.speed * dt;
     }
     // Check for player collision
+    // if they collide - send player to start position
     if (this.y === player.y) {
         if (this.x + 81 > player.x && this.x < player.x + 71) player.startPosition();
     }
@@ -42,10 +51,13 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
 var Player = function() {
+    // variables for the x and y position of the player
     this.x;
     this.y;
     this.sprite = 'images/char-pink-girl.png';
+    // set position of where player starts
     this.startPosition();
 }
 Player.prototype.startPosition = function() {
@@ -58,6 +70,8 @@ Player.prototype.update = function(dt) {
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
+// used a switch statement to determine which direction the player will move
+// limit the movement to keep the player from going off screen
 Player.prototype.handleInput = function(direction) {
     switch(direction) {
         case 'left':
@@ -122,7 +136,7 @@ for (let i=0; i<5; i++) {
     allEnemies.push(new Enemy());
 }
 var player = new Player();
-
+// create an alert with player wins game
 function winAlert() {
     alert('You win!');
 }
